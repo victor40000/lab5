a2037ff626e09490ac968eaf9cce97105e873cac
victor40000 <victor40000@yandex.ru>
2019-04-27 08:38:48 +0300

    added html page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongoClient = require("mongodb").MongoClient;
 var db = new mongoClient("mongodb://localhost:27017/", {useNewUrlParser: true});
 var app = express();
+app.use(express.static(__dirname + "/static"));
 
 db.connect(function(err, client){
     if (err) return console.log(err);
@@ -10,3 +11,4 @@ db.connect(function(err, client){
         console.log('Example app listening on port 3000!');
     });
 });
+
