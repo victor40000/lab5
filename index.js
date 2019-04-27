e4bb1217b0821551bd07fe17d17bbd448aa9931f
victor40000 <victor40000@yandex.ru>
2019-04-27 08:28:17 +0300

    added connection to db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var mongoClient = require("mongodb").MongoClient;
+var db = new mongoClient("mongodb://localhost:27017/", {useNewUrlParser: true});
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+db.connect(function(err, client){
+    if (err) return console.log(err);
+    app.locals.collection = client.db("lab5").collection("sketch");
+    app.listen(3000, function(){
+        console.log('Example app listening on port 3000!');
+    });
 });
