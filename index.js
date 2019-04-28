d45d4204e237f2b0ccb808ca200ab466b355ee6e
victor40000 <victor40000@yandex.ru>
2019-04-28 11:23:05 +0300

    added sketch loading

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
 var express = require('express');
 var mongoClient = require("mongodb").MongoClient;
 var db = new mongoClient("mongodb://localhost:27017/", {useNewUrlParser: true});
-var app = express();
-app.use(express.static(__dirname + "/static"));
+var application = express();
+var jsonParser = express.json();
+application.use(express.static(__dirname + "/static"));
 
 db.connect(function(err, client){
     if (err) return console.log(err);
-    app.locals.collection = client.db("lab5").collection("sketch");
-    app.listen(3000, function(){
+    application.locals.collection = client.db("lab5").collection("sketch");
+    application.listen(3000, function(){
         console.log('Example app listening on port 3000!');
     });
 });
 
+application.post("/sketch", jsonParser, function(request, result){
+    if(!request.body) return result.sendStatus(400);
+    console.log(request.body);
+    var title = request.body.title;
+    var text = request.body.text;
+    var collection = request.app.locals.collection;
+    collection.insertOne({title: title, text: text}, function(err, res){
+        if(err) return console.log(err);
+        result.send("Sketch has been added");
+    });
+});
+
+application.put("/sketch", jsonParser, function(request, result){
+    if(!request.body) return result.sendStatus(400);
+    // console.log(request.body);
+    var title = request.body.title;
+    var text = request.body.text;
+    var id = new objectId(request.body.id);
+    var collection = request.app.locals.collection;
+    collection.findOneAndUpdate({_id: id}, { $set: {title: title, text: text}}, function(err, result) {
+        if (err) return console.log(err);
+        res.send("Markup has been modeified");
+    });
+});
+
+application.delete("/sketch", jsonParser, function(req, res){
+    var id = new objectId(req.body.id);
+    var collection = req.app.locals.collection;
+    collection.findOneAndDelete({_id: id}, function(err, result){
+        if (err) return console.log(err);    
+        res.send("Draft has been deleted");
+    });
+    
+});
+
+application.get("/sketch", function(req, res) {
+    var collection = req.app.locals.collection;
+    collection.find({}).toArray(function(err, sketches){
+        if (err) return console.log(err);
+        res.send(sketches);
+    }); 
+});
--- static/script.js
+++ static/script.js
@@ -1,8 +1,86 @@
-var bt = new Vue({
-    el: '#button',
+var currentId;
+var currentTitle;
+var currentText;
+
+var addButton = new Vue({
+    el: '#addButton',
     methods: {
-        click: function () {
-            console.log("123");
+        addSketch: function () {
+            
         }
     }
 });
+
+
+var sketchList = new Vue({
+    el: '#sketchList',
+    data: {
+        sketches: null
+    }
+});
+
+function getSketches(){
+    $.ajax({
+        url: "/sketch",
+        contentType: "application/json",
+        method: "GET",
+        success: function (res) {
+            console.log(res);
+            sketchList.sketches = res;
+        }
+    })
+}
+
+getSketches();
+
+
+function postSketch(title, text) {
+    $.ajax({
+        url: "/sketch",
+        contentType: "application/json",
+        method: "POST",
+        data: JSON.stringify({
+            title: title,
+            text: text,
+        }),
+        success: function (res) {
+            // console.log(res);
+            location.href = "/";
+        }
+    })
+}
+
+function updateSketch(id, title, text) {
+    $.ajax({
+        url: "/sketch",
+        contentType: "application/json",
+        method: "PUT",
+        data: JSON.stringify({
+            id: id,
+            title: title,
+            text: text
+        }),
+        success: function (res) {
+            // console.log(res);
+            location.href = "/";
+        }
+    })
+}
+
+function deleteSketch(id) {
+    $.ajax({
+        url: "/sketch",
+        contentType: "application/json",
+        method: "DELETE",
+        data: JSON.stringify({
+            id: id
+        }),
+        success: function (res) {
+            // console.log(res);
+            location.href = "/";
+        }
+    })
+}
+
+
+
